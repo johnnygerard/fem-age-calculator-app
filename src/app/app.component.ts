986a557a6e4cfb4e8eaff07cc91681b221effd69
Johnny Gérard <john@jgerard.dev>
2024-01-12 02:10:14 +0100

    feat: add age computation logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+const EPOCH = 1583; // First full Gregorian year
+type Age = {
+  years: number;
+  months: number;
+  days: number;
+};
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,4 +16,46 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  /**
+   * @param month Zero-based month index
+   * @param year Year
+   * @returns Total number of days in the month
+   */
+  #getMonthDays(year: number, month: number): number {
+    // Day 0 retrieves the last day of the previous month
+    return new Date(year, month, 0).getDate();
+  }
+
+  computeAge(year: number, month: number, day: number): Age {
+    const today = new Date();
+    const birthDate = new Date(year, month, day);
+
+    let years = today.getFullYear() - birthDate.getFullYear();
+    let months = today.getMonth() - birthDate.getMonth();
+    let days = today.getDate() - birthDate.getDate();
+
+    if (months < 0) {
+      years--;
+      months += 12;
+    }
+
+    if (days < 0) {
+      months--;
+      days += this.#getMonthDays(today.getFullYear(), today.getMonth() - 1);
+    }
+
+    return { years, months, days };
+  }
+
+  isAgeNegative(age: Age): boolean {
+    return age.years < 0 || age.months < 0 || age.days < 0;
+  }
+
+  isValidDate(year: number, month: number, day: number): boolean {
+    const date = new Date(year, month, day);
+
+    return date.getDate() === day
+      && date.getMonth() === month
+      && date.getFullYear() === year;
+  }
 }
